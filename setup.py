8ddd41a34c19025424499a359d158b3e367a7b79
Simon Ramstedt <simonramstedt@gmail.com>
2018-08-30 15:36:40 -0400

    remove-numpy-dep

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 from setuptools import find_packages
 
-
 import sys
-# if sys.version_info < (3, 6):
-#   sys.exit('Sorry, Python < 3.6 is not supported')
-
 
 setup(name='yoke',
-      version='0.1',
+      version='0.1.1',
       description='',
       author='Simon Ramstedt',
       author_email='simonramstedt@gmail.com',
