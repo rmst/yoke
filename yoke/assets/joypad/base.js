aedebf42372a92b480e23a2a98daf32ab6f7410b
medape <medape@gmx.com>
2019-07-28 20:55:35 +0200

    Clean up JS code
    Controls are now sorted and chosen within different functions, some lines are rearranged and others are simplified. Also, unrecognised controls are now discarded with a warning, but don't generate JS errors.

--- yoke/assets/joypad/base.js
+++ yoke/assets/joypad/base.js
@@ -1,24 +1,27 @@
 'use strict';
 
-// Settings:
+// SETTINGS:
 var VIBRATE_ON_QUADRANT_BOUNDARY = true;
 var VIBRATE_ON_PAD_BOUNDARY = true;
-
-// Code:
-
 // These 2 options are recommended for testing in non-kiosk/non-embedded browsers:
 var WAIT_FOR_FULLSCREEN = true;
 var DEBUG_NO_CONSOLE_SPAM = true;
 
+// CONSTANTS:
 var VIBRATION_MILLISECONDS_IN = 40;
 var VIBRATION_MILLISECONDS_OUT = 30;
 var VIBRATION_MILLISECONDS_OVER = 40;
 var VIBRATION_MILLISECONDS_SATURATION = [10, 10];
 var ACCELERATION_CONSTANT = 0.025;
 
-//
-// MISCELLANEOUS HELPER FUNCTIONS
-//
+// HELPER FUNCTIONS:
+// Within the Yoke webview, Yoke.update_vals() sends the joypad state.
+// Outside the Yoke webview, Yoke.update_vals() is redefined to have no effect.
+// This prevents JavaScript exceptions, and wastes less CPU time when in Yoke:
+if (typeof window.Yoke === 'undefined') {
+    window.Yoke = {update_vals: function() {}};
+}
+
 function prettyAlert(message) {
     var warningDiv = document.getElementById('warning');
     warningDiv.innerHTML = message + '<p class=\'dismiss\'>Tap to dismiss.</p>';
@@ -26,104 +29,77 @@ function prettyAlert(message) {
 }
 
 // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates#14438954
-function unique(value, index, self) {
-    return self.indexOf(value) === index;
+function unique(value, index, self) { return self.indexOf(value) === index; }
+
+function mnemonics(id, callback) {
+    // Chooses the correct control for the joypad from its mnemonic code.
+    switch (id[0]) {
+        case 's': case 'j':
+            // 's' is a locking joystick, 'j' - non-locking
+            return new Joystick(id, callback); break;
+        case 'm':
+            var legalLabels = 'xyzabg';
+            if (legalLabels.indexOf(id[1]) == -1) {
+                prettyAlert('Motion detection error: \
+                    Unrecognised coordinate <code>' + id[1] + '</code>.');
+                return null;
+            } else { return new Motion(id, callback); }
+            break;
+        case 'p':
+            var legalLabels = 'abt';
+            if (legalLabels.indexOf(id[1]) == -1) {
+                prettyAlert('<code>' + id + '</code> is not a valid pedal. \
+                    Please use <code>pa</code> or <code>pt</code> for accelerator \
+                    and <code>pb</code> for brakes.');
+                return null;
+            } else { return new Pedal(id, callback); }
+            break;
+        case 'k': return new Knob(id, callback); break;
+        case 'a': return new AnalogButton(id, callback); break;
+        case 'b': return new Button(id, callback); break;
+        case 'd':
+            var legalLabels = 'udlr';
+            if (legalLabels.indexOf(id[1]) == -1) {
+                prettyAlert('D-pad error: \
+                    <code>' + id[1] + '</code> is not a cardinal direction.');
+                return null;
+            } else { return new Button(id, callback); break; }
+        default:
+            prettyAlert('Unrecognised control <code>' + id + '</code> at user.css.');
+            return null; break;
+    }
 }
 
-function mnemonics(a, b) {
-    var callback, ids;
-    if (typeof b == 'function') {
-        // if b is a callback function, mnemonics() chooses the correct control for the joypad
-        callback = b;
-        ids = [a];
-    } else {
-        // if b is a string, mnemonics() serves as a custom sorting algorithm
-        callback = null;
-        ids = [a, b];
-    }
+function categories(a, b) {
+    // Custom algorithm to sort control mnemonics.
+    var ids = [a, b];
     var sortScores = ids.slice();
-    // sortScores contains arbitrary numbers.
-    // These only matter if mnemonics() is being used as a sort function;
-    // the lower-ranking controls are attached earlier.
+    // sortScores contains arbitrary numbers. The lower-ranking controls are attached earlier.
     ids.forEach(function(id, i) {
         if (id == 'dbg') { sortScores[i] = 999998; } else {
             sortScores[i] = 999997;
             switch (id[0]) {
-                case 's': case 'j':
-                    // 's' is a locking joystick, 'j' - non-locking
-                    if (typeof callback == 'function') {
-                        sortScores[i] = new Joystick(id, callback);
-                    } else { sortScores[i] = 100000; }
-                    break;
-                case 'm':
-                    if (typeof callback == 'function') {
-                        sortScores[i] = new Motion(id, callback);
-                        switch (id[1]) {
-                            case 'x': case 'y': case 'z': case 'a': case 'b': case 'g': break;
-                            default:
-                                prettyAlert('Motion detection error: \
-                                    Unrecognised coordinate <code>' + id[1] + '</code>.');
-                                break;
-                        }
-                    } else { sortScores[i] = 200000; }
-                    break;
-                case 'p':
-                    if (typeof callback == 'function') {
-                        sortScores[i] = new Pedal(id, callback);
-                        switch (id[1]) {
-                            case 'a': case 'b': case 't': break;
-                            default:
-                                prettyAlert('<code>' + id + '</code> is not a valid pedal. \
-                                    Please use <code>pa</code> or <code>pt</code> for accelerator \
-                                    and <code>pb</code> for brakes.');
-                                break;
-                        }
-                    } else { sortScores[i] = 300000; }
-                    break;
-                case 'k':
-                    if (typeof callback == 'function') {
-                        sortScores[i] = new Knob(id, callback);
-                    } else { sortScores[i] = 400000; }
-                    break;
-                case 'a':
-                    if (typeof callback == 'function') {
-                        sortScores[i] = new AnalogButton(id, callback);
-                    } else { sortScores[i] = 500000; }
-                    break;
-                case 'b':
-                    if (typeof callback == 'function') {
-                        sortScores[i] = new Button(id, callback);
-                    } else { sortScores[i] = 600000; }
-                    break;
-                case 'd':
-                    if (typeof callback == 'function') {
-                        sortScores[i] = new Button(id, callback);
-                        switch (id[1]) {
-                            case 'u': case 'd': case 'l': case 'r': break;
-                            default:
-                                prettyAlert('D-pad error: \
-                                    <code>' + id[1] + '</code> is not a cardinal direction.');
-                                break;
-                        }
-                    } else { sortScores[i] = 700000; }
-                    break;
-                default:
-                    sortScores[i] = 999999;
-                    prettyAlert('Unrecognised control <code>' + id + '</code> at user.css.');
-                    break;
+                // 's' is a locking joystick, 'j' - non-locking
+                case 's': case 'j': sortScores[i] = 100000; break;
+                case 'm': sortScores[i] = 200000; break;
+                case 'p': sortScores[i] = 300000; break;
+                case 'k': sortScores[i] = 400000; break;
+                case 'a': sortScores[i] = 500000; break;
+                case 'b': sortScores[i] = 600000; break;
+                case 'd': sortScores[i] = 700000; break;
+                default: sortScores[i] = 999999999; break;
             }
             if (sortScores[i] < 999990) {
                 // This line should sort controls in the same category by id length,
                 // and after that, by the ASCII codes in the id tag
-                // (the first letter may be different without changing the category)
-                // This shortcut reorders non-negative integers correctly, and letters with the same capitalization in alphabetical order.
+                // This shortcut reorders non-negative integers at the end of a mnemonic correctly,
+                // and letters with the same capitalization in alphabetical order.
                 sortScores[i] += id.substring(1).split('')
                     .reduce(function(acc, cur) {return 256 * acc + cur.charCodeAt();}, 0);
             }
         }
     });
-    if (typeof callback == 'function') { return sortScores[0]; }
-    else { return (sortScores[0] < sortScores[1]) ? -1 : 1; }
+    return (sortScores[0] < sortScores[1]) ? -1 : 1;
 }
 
 function truncate(f, id, pattern) {
@@ -139,18 +115,26 @@ function truncate(f, id, pattern) {
     return f;
 }
 
-// Functions to mix haptic feedback from every element at a high level.
-
+// HAPTIC FEEDBACK MIXING:
 var vibrating = {};
 
 function queueForVibration(id, pattern) {
-    if (!(id in vibrating)) {vibrating[id] = {time: performance.now(), pulse: pattern[0], pause: pattern[1], kill: false};}
+    if (!(id in vibrating)) {
+        vibrating[id] = {
+            time: performance.now(),
+            pulse: pattern[0],
+            pause: pattern[1],
+            kill: false
+        };
+    }
 }
+
 function unqueueForVibration(id) {
-    if (id in vibrating) {vibrating[id].kill = true;}
+    if (id in vibrating) { vibrating[id].kill = true; }
     // And wait for checkVibration to kill it.
     // This is heavier for the browser, but also avoids race conditions between checkVibration() and unqueueForVibration().
 }
+
 function checkVibration() {
     for (var id in vibrating) {
         if (vibrating[id].kill) {
@@ -163,9 +147,7 @@ function checkVibration() {
     window.requestAnimationFrame(checkVibration);
 }
 
-//
-// CONTROL DEFINITIONS
-//
+// GAMEPAD CONTROLS:
 function Control(type, id, updateStateCallback) {
     this.element = document.createElement('div');
     this.element.className = 'control ' + type;
@@ -174,6 +156,7 @@ function Control(type, id, updateStateCallback) {
     this.gridArea = id;
     this.updateStateCallback = updateStateCallback;
     this._state = 0;
+    this.shape = 'rectangle';
 }
 Control.prototype.getBoundingClientRect = function() {
     this._offset = this.element.getBoundingClientRect();
@@ -181,6 +164,19 @@ Control.prototype.getBoundingClientRect = function() {
     this._offset.semiheight = this._offset.height / 2;
     this._offset.xCenter = this._offset.x + this._offset.semiwidth;
     this._offset.yCenter = this._offset.y + this._offset.semiheight;
+    if (this.shape == 'square') {
+        if (this._offset.width < this._offset.height) {
+            this._offset.y += this._offset.semiheight - this._offset.semiwidth;
+            this._offset.height = this._offset.width;
+            this._offset.semiheight = this._offset.semiwidth;
+        } else {
+            this._offset.x += this._offset.semiwidth - this._offset.semiheight;
+            this._offset.width = this._offset.height;
+            this._offset.semiwidth = this._offset.semiheight;
+        }
+    }
+    this._offset.xMax = this._offset.x + this._offset.width;
+    this._offset.yMax = this._offset.y + this._offset.height;
 };
 Control.prototype.onAttached = function() {};
 Control.prototype.state = function() {
@@ -254,18 +250,8 @@ function Motion(id, updateStateCallback) {
     // but maybe it's not the most performant.
     // Motion calculates always every coordinate, then applies a mask on it.
     if (id.length != 2) { prettyAlert('Please use only one coordinate per motion sensor.'); }
-    this._mask = null;
-    switch (id[1]) {
-        case 'x': this._mask = 0; break;
-        case 'y': this._mask = 1; break;
-        case 'z': this._mask = 2; break;
-        case 'a': this._mask = 3; break;
-        case 'b': this._mask = 4; break;
-        case 'g': this._mask = 5; break;
-        default:
-            prettyAlert('Motion detection error: Unrecognised coordinate <code>' + id[1] + '</code>.');
-            break;
-    }
+    var legallabels = 'xyzabg';
+    this._mask = legallabels.indexOf(id[1]);
     // Only the last defined sensor sends events.
     // It's a really hacky and ugly method, but all the motion information
     // is a property of the window anyways, not of any Motion instance.
@@ -395,6 +381,7 @@ AnalogButton.prototype.onTouchEnd = function() {
 
 function Knob(id, updateStateCallback) {
     Control.call(this, 'knob', id, updateStateCallback);
+    this.shape = 'square';
     this._state = 0;
     this._knobcircle = document.createElement('div');
     this._knobcircle.className = 'knobcircle';
@@ -407,15 +394,6 @@ function Knob(id, updateStateCallback) {
 Knob.prototype = Object.create(Control.prototype);
 Knob.prototype.onAttached = function() {
     // Centering the knob within the boundary.
-    if (this._offset.width < this._offset.height) {
-        this._offset.y += this._offset.semiheight - this._offset.semiwidth;
-        this._offset.height = this._offset.width;
-        this._offset.semiheight = this._offset.semiwidth;
-    } else {
-        this._offset.x += this._offset.semiwidth - this._offset.semiheight;
-        this._offset.width = this._offset.height;
-        this._offset.semiwidth = this._offset.semiheight;
-    }
     this._knobcircle.style.top = this._offset.y + 'px';
     this._knobcircle.style.left = this._offset.x + 'px';
     this._knobcircle.style.height = this._offset.width + 'px';
@@ -454,7 +432,7 @@ Knob.prototype._updateCircles = function() {
 
 function Button(id, updateStateCallback) {
     Control.call(this, 'button', id, updateStateCallback);
-    this._state = false;
+    this._state = 0;
     buttons += 1;
 }
 Button.prototype = Object.create(Control.prototype);
@@ -465,24 +443,20 @@ Button.prototype.onAttached = function() {
 };
 Button.prototype.onTouchStart = function(ev) {
     ev.preventDefault(); // Android Webview delays the vibration without this.
-    this._state = true;
+    this._state = 1;
     this.updateStateCallback();
     this.element.classList.add('pressed');
     window.navigator.vibrate(VIBRATION_MILLISECONDS_IN);
 };
 Button.prototype.onTouchEnd = function() {
-    this._state = false;
+    this._state = 0;
     this.updateStateCallback();
     this.element.classList.remove('pressed');
     window.navigator.vibrate(VIBRATION_MILLISECONDS_OUT);
 };
-Button.prototype.state = function() {
-    return (this._state ? 1 : 0);
-};
+Button.prototype.state = function() { return this._state; };
 
-//
-// JOYPAD
-//
+// JOYPAD:
 function Joypad() {
     var updateStateCallback = this.updateState.bind(this);
 
@@ -493,11 +467,18 @@ function Joypad() {
         .gridTemplateAreas
         .split('"').join('').split(' ')
         .filter(function(x) { return x != '' && x != '.'; });
-    var controlIDs = gridAreas.sort(mnemonics).filter(unique);
+    var controlIDs = gridAreas.sort(categories).filter(unique);
     this._debugLabel = null;
     controlIDs.forEach(function(id) {
         if (id != 'dbg') {
             this._controls.push(mnemonics(id, updateStateCallback));
+            if (this._controls[this._controls.length - 1] == null) {
+                this._controls.pop() // discard unrecognised controls
+            }
+            this.updateDebugLabel = function(state) {
+                // shadow dummy function with useful function
+                this._debugLabel.element.innerHTML = state;
+            }
         } else if (this._debugLabel == null) {
             this._debugLabel = new Control('debug', 'dbg');
             this.element.appendChild(this._debugLabel.element);
@@ -521,29 +502,14 @@ function Joypad() {
 }
 Joypad.prototype.updateState = function() {
     var state = this._controls.map(function(control) { return control.state(); }).join(',');
-
-    // Within the Yoke webview, sends the joypad state.
-    // Outside the Yoke webview, window.Yoke.update_vals() is redefined to have no effect.
-    // This prevents JavaScript exceptions, and wastes less CPU time when in Yoke:
     window.Yoke.update_vals(state);
-
-    if (this._debugLabel != null) {
-        this._debugLabel.element.innerHTML = state;
-    }
-
+    this.updateDebugLabel(state);
     if (!DEBUG_NO_CONSOLE_SPAM) { console.log(state); }
 };
+Joypad.prototype.updateDebugLabel = function() { } //dummy function
 
-//
-// BASE CODE
-//
-
-// Dummy Yoke.update_vals function.
-if (typeof window.Yoke === 'undefined') {
-    window.Yoke = {update_vals: function() {}};
-}
-
-// These variables are automatically updated by the code
+// BASE CODE:
+// These variables are automatically updated by the code:
 var joypad = null;
 var buttons = 0; // number of buttons total
 var axes = 0; // number of analog axes total
